514f9fba69ffb19fc7c52fad62450572ec01b0d6
Jan-Petter Gundersen <europe.jpg@gmail.com>
2016-03-18 18:56:34 +0100

    Noindex test

---
+++ test/cases/NoindexTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace vipnytt\XRobotsTagParser\tests;
+
+use vipnytt\XRobotsTagParser;
+
+class NoindexTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * noindex test
+     *
+     * @dataProvider generateDataForTest
+     * @param string $url
+     * @param string $bot
+     * @param array $options
+     */
+    public function testNoIndex($url, $bot, $options)
+    {
+        $parser = new XRobotsTagParser($url, $bot, $options);
+        $this->assertInstanceOf('vipnytt\XRobotsTagParser', $parser);
+
+        $this->assertTrue($parser->getRules(true)['noindex']);
+        $this->assertTrue($parser->getRules(false)['noindex']);
+        $this->assertTrue($parser->getRules(false)['noarchive']);
+    }
+
+    /**
+     * Generate test data
+     * @return array
+     */
+    public function generateDataForTest()
+    {
+        return [
+            [
+                'http://example.com/',
+                'googlebot',
+                ['headers' =>
+                    [
+                        'X-Robots-Tag: noindex'
+                    ]
+                ]
+            ]
+        ];
+    }
+}
